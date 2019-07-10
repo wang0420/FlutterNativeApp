ffca21087985295b4ac2c1744be91a28548e78ec
王伟 <wei.wang23@zhenai.com>
2019-07-10 18:09:09 +0800

    qianyi

--- app/src/main/java/com/demo/app/BaseApplication.java
+++ app/src/main/java/com/demo/app/BaseApplication.java
@@ -2,8 +2,6 @@ package com.demo.app;
 
 import android.app.Application;
 
-import io.flutter.view.FlutterMain;
-
 /**
  * Created by wangwei on 2019/7/1.
  */
@@ -12,6 +10,6 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FlutterMain.startInitialization(this);
+        //FlutterMain.startInitialization(this);
     }
 }
